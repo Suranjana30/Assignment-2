23a966d1a941b62461050905c304f5a1506d3697
Suranjana30 <90706866+Suranjana30@users.noreply.github.com>
2022-01-19 19:28:57 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,27 @@
+#list
+Items = [23,46]
+Items.append(45)#Insert any item
+print(Items)
+Items.pop()#Delete any item
+print(Items)
+Items1=[32,78]
+Items=Items+Items1
+print(Items)
+print(Items1[0])
+print(len(Items))#return number of items in the list
+print(max(Items))#return maximum element in the list
+print(min(Items))#return minimum element in the list
+print(sum(Items))#return sum of all elements in the list
+print(sorted(Items))#return sorted list,list remains unchanged
+Items.reverse()#used for reversing list
+print(Items)
+#tuple
+a = ()
+b=(10,)
+tpl1 = (10,)*5
+tpl2 = (10)*5
+print(tpl1)
+print(tpl2)
+#set
+s ={12,23,45}
+print(s)
